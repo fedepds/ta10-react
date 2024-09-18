3c340f379b91a516fe939439547845770bde8297
Federico Pizarro <127800142+fedepds@users.noreply.github.com>
2024-09-18 20:37:55 -0300

    TA10_terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const getUser = async () => {
+  return fetch('https://jsonplaceholder.typicode.com/users').then(resp => resp.json());
+};
+
+const ShowUsers = ({ users }) => {
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>
+          {user.email} {user.username}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUser().then(usersData => setUsers(usersData));
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de Usuarios</h1>
+      <ShowUsers users={users} />
     </div>
   );
 }
